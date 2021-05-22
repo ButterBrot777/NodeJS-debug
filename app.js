0fed11b468f874356b60897eff0732ce4c78d209
Pavel Karenda <korendos@gmail.com>
2021-05-22 22:32:43 +0300

    feat: refactor and add endpoints

--- app.js
+++ app.js
@@ -3,21 +3,16 @@ const PORT = process.env.PORT || 5433
 const app = express();
 const bodyParser = require('body-parser');
 
-const db = require('./db');
 const user = require('./controllers/usercontroller');
-const game = require('./controllers/gamecontroller')
+const game = require('./controllers/gamecontroller');
+const session = require('./middleware/validate-session');
 
-db.sequelize.sync();
-// db.sequelize.sync().then(result=>{
-//     console.log(result);
-// });
 app.use(bodyParser.json());
 
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
-
+app.use(session);
 app.use('/api/game', game);
+
 app.listen(PORT,function() {
-    console.log("App is listening on 4000");
-    console.log("PORT: ", PORT);
+    console.log(`App is listening on ${PORT}`);
 })
